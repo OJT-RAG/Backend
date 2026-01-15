eb016ee0b395ed5e362087a02b82c2cfec260fc3
TrongNhan <144611793+trongnhan0312@users.noreply.github.com>
2026-01-15 17:46:55 +0700

    fix

--- OJT_RAG.Repositories/Entities/Companydocument.cs
+++ OJT_RAG.Repositories/Entities/Companydocument.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -10,6 +10,7 @@ namespace OJT_RAG.Repositories.Entities
     public partial class Companydocument
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Column("companydocument_id")]
         public long CompanydocumentId { get; set; }
 
